921c1656727e3156da90855071715b1ff74a0e89
Cameron Smith <71192028+smith-cameron@users.noreply.github.com>
2024-05-02 09:10:53 -0700

    W1D2

--- fundamentalsPt2.py
+++ fundamentalsPt2.py
@@ -3,21 +3,20 @@
 # < a named block of code that we can execute to perform a specific task
 # < we can run at any time and as many times as required
 
-def function_name():
+def function_name(input):
     # code body
-    # to work here
+    # do work here
+    print(input)
     print("hello_world from inside the function code block")
-    return "return anything from inside the function we like"
-
-# x = function_name()
-# print(x)
+    return f"return anything from inside the function we like: {input}"
+# return_value = function_name(11)
+# print(return_value)
 
 # _ Conditionals
 # < if, elif, else (conditional statements)
 # < Conditional statements allow us to run certain lines of code
     # < depending on whether certain conditions are met.
 # < These statements control the flow of how our code is executed
-
 def conditionals():
     x = 12
     if x > 50:
@@ -37,22 +36,32 @@ def conditionals():
 
     if x < 10:
         print("smaller than 10")
-    # nothing happens, because the statement is false
-# conditionals()
+    # nothing happens, because the statement is false   
 
-def conditions_with_params(num1=0, num2=0):
-    print(f"input param num1: {num1}")
-    print(f"input param num2: {num2}")
-    if num1 > num2:
+def conditionals_with_params(input1 = 12, input2 = 7):
+    if input1 > input2:
+        # If statements are ALWAYS CHECKED
+        # This block will only run if condition is truthy
         print("if")
-        print(f"{num1} bigger than {num2}")
-    elif num1 < num2:
+        print(f"{input1} is bigger than {input2}")
+    elif input1 < input2:
+        # elif statements are checked IF ABOVE IS falsy
+        # This block will only run if condition is truthy
         print("elif")
-        print(f"{num2} bigger than {num1}")
+        print(f"{input2} is bigger than {input1}")
     else:
+        # Will run ONLY if all conditions above are falsy
         print("else")
         print("they are equal")
-# conditions_with_params(0)
+# conditionals_with_params(input2 = 5,input1 = 6)
+
+# < Named Arguments & Default Parameters
+#* no arguments are provided -- the defaults are used
+#* one unnamed argument provided -- provided value is used as the value for the first parameter, and the second parameter's default value is used
+#* one named argument provided -- provided value is used as the value of the parameter of the same name, and the other parameter's default value is used
+#* both unnamed arguments provided -- values assigned to parameters in order (i.e. what we've been doing up to this point)
+#* both named arguments provided -- values are assigned to associated parameters (and then order doesn't matter!)
+
 
 # _ Lists
 # < an ordered and mutable collection of items.
@@ -60,54 +69,46 @@ def conditions_with_params(num1=0, num2=0):
 # < Element values can be anything - even more composite types
 # < 0 base index
 
-numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-words = ["this", "is", "a", "list", "of", "strings"]
-mash_up = ['string', 4.65, 3, True, "another_string", numbers]
-#    0      1    2      3         4              5
+# > SYNTAX => list_name = [value, value, value]
+words = ["start","going","till","the","end"]
+numbers= [1,2,3,4,5,6,7,8,9]
+mix_up = ["start",6,[1,2,3,4,5],numbers,True, 22.45,"end"]
+        #   0     1     2        3      4      5     6
+# print(len(mix_up))
 
 # > Accessing Values:
 def accessing_list_values():
+    # print(mix_up[3])
     # * print(list_name[index])
-    print(mash_up[3])
-
+    
     # > Replacing values
     # * list_name[index] = new_value
-    mash_up[3] = "no longer a bool"
-    print(mash_up[3])
-    print(mash_up)
+    mix_up[6] = "finished"
+    # print(mix_up)
 
     # > Store value temporarily
     # * temp = list_name[index]
-    temp = mash_up[3]
+    temp = mix_up[5]
     print(temp)
-    mash_up[3] = False
-    print(mash_up)
-    print(temp)
-    mash_up[2] = temp
-    print(mash_up)
-
+    mix_up[5] = mix_up[1]
+    mix_up[1] = temp
+    
     # > Replace a value with another value
     # * list_name[index] = list_name[other_index]
-    print(numbers)
-    numbers[0] = numbers[2]
-    print(numbers)
-
+    mix_up[5] = mix_up[1]
+    
     # > Adding values to a list
     # * list_name.append(new_value)
-    numbers.append(9)
-    print(numbers)
-
+    words.append("just kiddingggg")
+    
     # > Removing values of a list
     # * list_name.pop()
     # * list_name.pop(0)
-    numbers.pop()
-    print(numbers)
-    numbers.pop(4)
-    print(numbers)
-
+    words.pop()
+    words.pop(0)
+    
     # > Number of Elements
-    # * print(len(list_name))
-    print(len(numbers))
+    print(len(mix_up))
 # accessing_list_values()
 
 # ? List vs. Tuple
@@ -116,13 +117,10 @@ def accessing_list_values():
 
 def tuple_stuff():
     my_tuple = ('this is a tuple', True, [1, 2, 3, 4, 5])
-    print(my_tuple)
     my_list = ['this is a tuple', False, [1, 2, 3, 4, 5]]
-    print(my_list)
     import sys
-    # < tuple take less memory becasue they are immutable
-    print("Byte size of the list:", sys.getsizeof(my_list))
-    print("Byte size of the tuple:", sys.getsizeof(my_tuple))
+    print("Byte size of tuple", sys.getsizeof(my_tuple))
+    print("Byte size of list", sys.getsizeof(my_list))
 # tuple_stuff()
 
 # _ Loops
@@ -130,32 +128,45 @@ def tuple_stuff():
 # < Python supports two main types of loops: `for` loops and `while` loops
 
 # > FOR IN
-def for_in():
-    for unicorn in mash_up:
+#,  for oneThing in manyThings:
+#,      print(do some stuff)
+def for_in_loop():
+    for unicorn in numbers:
         print(unicorn)
-# for_in()
-
+# for_in_loop()
+def for_in_string_loop():
+    for unicorn in "all the beautiful unicorns":
+        print(unicorn)
+# for_in_string_loop()
 def nested_for_in():
     for unicorn in words:
         print(unicorn)
-        for i in unicorn:
-            print(i)
+        for horn in unicorn:
+            print(horn)
 # nested_for_in()
 
 # > FOR IN RANGE
 # < ITERATOR, START, STOP/BOUNDARY, STEP (DEFINED HORIZONTALY)
-def in_range():
-    print(numbers)
-    for i in range(0, len(numbers), 1):
+
+#,  for iteratorVariable in range(start, stop, step):
+#, 	    print(do some stuff)
+def for_in_range():
+    print(len(mix_up)+1)
+    for i in range(0, len(mix_up),1):
         print(f"i: {i}")
-        print(f"value: {numbers[i]}")
-# in_range()
+        print(f"value: {mix_up[i]}")
+# for_in_range()
 
 # > WHILE
 # < ITERATOR, START, STOP/BOUNDARY, STEP (DEFINED VERTICALLY)
-def while_loops():
+
+#,  iteratorVariable = start
+#,  while (conditional_boundary):
+#, 	    # do some stuff
+#, 	    iteratorVariable += step
+def while_loop():
     i = 10
     while (i <= 20):
-        print(f"i: {i}")
+        print(i)
         i += 1
-# while_loops()
+# while_loop()
